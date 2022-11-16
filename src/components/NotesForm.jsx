1b527b15e5f029edec305f4f2275bc9b07e5e72b
Utsav <utsavpoddar99@gmail.com>
2022-11-16 19:17:15 +0530

    real time validation

--- src/components/NotesForm.jsx
+++ src/components/NotesForm.jsx
@@ -1,13 +1,38 @@
 import {useState} from "react"
 function NotesForm() {
     const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState(""); 
 
-  return ( 
+    const handleTextChange = (e) => {
+      
+      if(text === ""){
+        setBtnDisabled(true)
+        setMessage(null)
+      }
+
+      else if (text !== null && text.trim().length <= 10){
+        setBtnDisabled(true)
+        setMessage("6969696969")
+      }
+
+      else {
+
+        setMessage(null)
+        setBtnDisabled(false);
+      }
+
+      setText(e.target.value)
+    }
+
+  return (   
     <div>
         <form>
             <h2>Rate your note</h2>  
-            <input type="text" placeholder="Write a note" />
-            <button type="Submit"> Send</button>
+            <input onChange={handleTextChange} type="text" placeholder="Write a note" />
+            <button disabled={btnDisabled} type="Submit"> Send</button>
+
+            {message && <div> {message} </div> }
         </form>
     </div>
   )
